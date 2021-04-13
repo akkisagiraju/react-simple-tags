a37f9bc6146e66be89e0b898d40064898f8753ec
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2021-04-14 00:10:52 +0530

    Updates

---
+++ precache-manifest.4e0b0a6d0b3f717b022e9bfbf826dc62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df53565d2da43fe60cf7e07dc6bb3689",
+    "url": "./index.html"
+  },
+  {
+    "revision": "62fe290ea5c32323be57",
+    "url": "./static/css/main.6e5dc995.chunk.css"
+  },
+  {
+    "revision": "ebe3df00c6a56ee19e26",
+    "url": "./static/js/2.1a70bda2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.1a70bda2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62fe290ea5c32323be57",
+    "url": "./static/js/main.9290d0a8.chunk.js"
+  },
+  {
+    "revision": "08693d2826bc36942c51",
+    "url": "./static/js/runtime-main.d13d6131.js"
+  }
+]);
